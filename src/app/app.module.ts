228d149a657954ade8e7f88629c5c63650ce062c
Mitin <errocksc@gmail.com>
2017-08-21 15:02:14 -0400

    Firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FIREBASE_CREDENTIALS } from './firebase.credentials'
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CREDENTIALS)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyA76h85-3uDRMbLxUGuaCJ5vq3_l8zOdXs",
+    authDomain: "shoppinglist-86eb8.firebaseapp.com",
+    databaseURL: "https://shoppinglist-86eb8.firebaseio.com",
+    projectId: "shoppinglist-86eb8",
+    storageBucket: "shoppinglist-86eb8.appspot.com",
+    messagingSenderId: "446027725347"
+  }
